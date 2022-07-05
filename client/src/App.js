c77dac788ccbad9f96ce3b3a7f2dc9b388b72d12
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-07-05 14:37:01 +0300

    App routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
+
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbarr from "./components/navbar";
+import Movies from "./components/movies";
+import Favorites from "./components/Favorites";
 
 function App() {
-  return <div className="App">Welcome APP</div>;
+  return <div className="App"> <br/>
+<Navbarr/>
+    <Routes>
+      <Route path={"/movies"} element={<Movies />} />
+      <Route path={"/favorites"} element={<Favorites/>}/>
+    </Routes>
+
+  </div>;
 }
 
 export default App;
