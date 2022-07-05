3a96c92df1dc36927ba34baa235d494c8434f458
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-07-05 14:39:47 +0300

    OneMovie in OneMovie componant

---
+++ client/src/components/OneMovie/index.js
@@ -0,0 +1,15 @@
+// OneMovie
+
+import React, { useState, useEffect } from "react";
+
+const OneMovie = () => {
+
+
+  return (
+    <>
+     <div>OneMovie</div>
+    </>
+  );
+};
+
+export default OneMovie;
