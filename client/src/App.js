b69f9fa40205a41185cca154854ff0317c968c58
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-07-07 00:44:56 +0300

    stayling and paginnation for all movies

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,16 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Navbarr from "./components/navbar";
 import Movies from "./components/movies";
 import Favorites from "./components/Favorites";
+import OneMovie from "./components/OneMovie"
 
 function App() {
-  return <div className="App"> <br/>
+  return <div className="App">
 <Navbarr/>
     <Routes>
       <Route path={"/movies"} element={<Movies />} />
       <Route path={"/favorites"} element={<Favorites/>}/>
+      <Route path={"/OneMovie/:id"} element={<OneMovie/>}/>
     </Routes>
-
   </div>;
 }
 
