04a3ad6cc313fecb2c6a4371267410c2f4ac16ae
AhmadMohSal <ahmad.moh.sal@gmail.com>
2022-07-07 17:11:09 +0300

    one move stayling

--- client/src/components/OneMovie/index.js
+++ client/src/components/OneMovie/index.js
@@ -1,22 +1,20 @@
-// OneMovie
 import "./style.css";
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { MdOutlineFavoriteBorder } from "react-icons/md";
 
-const OneMovie = () => {
+const OneMovie = ({}) => {
   const navigate = useNavigate();
   const { id } = useParams();
   const [onemovie, setOnemovie] = useState("");
-
   const getOneMovie = () => {
     axios
       .get(
         `https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=1bfa430aada4409bfa6a3c5528128e8a`
       )
       .then((result) => {
-        // console.log(result.data);
         setOnemovie(result.data);
       })
       .catch((err) => {
@@ -33,56 +31,55 @@ const OneMovie = () => {
   //production_companies
   return (
     <>
-<section className="section about-section gray-bg" id="about">
+      <section className="section about-section gray-bg" id="about">
         <div className="container">
+          <p>
+            <AiOutlineStar />
+          </p>
           <div className="row align-items-center flex-row-reverse">
             <div className="col-lg-6">
               <div className="about-text go-to">
-                <h3 className="dark-color">{onemovie.title}</h3>
-          
                 <p>
-                  the  <mark> {onemovie.title} is  </mark>
-                  {onemovie.overview}                 </p>
+                  {" "}
+                  <h3 className="dark-color">{onemovie.title}</h3>
+                </p>
+                <p>
+                  the <mark>{onemovie.title} is </mark>
+                  {onemovie.overview}
+                </p>
                 <div className="row about-list">
                   <div className="col-md-6">
                     <div className="media">
-                      <label>rating</label>
-                      <label>{onemovie.vote_average} </label>
+                      <p>
+                        {" "}
+                        <label>rating</label> <p>{onemovie.vote_average}</p>
+                      </p>
                     </div>
-                    <div className="media">
-                      <label>release_date</label>
-                      <p>{onemovie.release_date}</p>
-                    </div>
-
                   </div>
+
                   <div className="col-md-6">
                     <div className="media">
-                      <label>E-mail</label>
-                      <p>ahmad.moh.sal@gmail.com</p>
-                    </div>
-                    <div className="media">
-                      <label>Phone</label> <p>962-790-</p>
-               
+                      <p>
+                        {" "}
+                        <label>release_date</label>{" "}
+                        <p>{onemovie.release_date}-</p>
+                      </p>
                     </div>
                   </div>
                 </div>
               </div>
+              <h1 style={{color:"wheat" ,cursor:"pointer"}}> <MdOutlineFavoriteBorder/></h1>
             </div>
             <div className="col-lg-6">
               <div className="about-avatar">
-              <img
-                      src={`https://image.tmdb.org/t/p/w500/${onemovie.poster_path} `}
-                    />
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${onemovie.poster_path} `}
+                />
               </div>
             </div>
           </div>
-          </div>
+        </div>
       </section>
-
-
-
-
-    
     </>
   );
 };
